a8a747f8f900a0132aac549a7799b1ff54428928
Dhulipallanaveena <81727940+Dhulipallanaveena@users.noreply.github.com>
2021-10-31 21:36:51 +0530

    Create Diamond of Stars.py

---
+++ Diamond of Stars.py
@@ -0,0 +1,23 @@
+n=int(input())
+k=0
+n=(n//2)+1
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(end=" ")
+    while k!=2*i-1:
+        print("*",end="")
+        k=k+1
+    k=0
+    print()
+    
+k=2
+l=1
+for i in range(1,n):
+    for j in range(1,k):
+        print(end=" ")
+    k=k+1
+    while l<=(2*(n-i)-1):
+        print("*",end="")
+        l=l+1
+    l=1
+    print()
